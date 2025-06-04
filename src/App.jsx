f3be4200198a18eb6c237100a8d66cfd94faca5e
petraalpigiano <petraalpigiano@gmail.com>
2025-06-04 16:30:36 +0200

    add basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import PostsPage from "./pages/posts/PostsPage";
+import DefaultLayout from "./pages/layouts/DefaultLayout";
+import HomePage from "./pages/HomePage";
+
 export default function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          {/* ROTTE CON IN COMUNE IL LAYOUT */}
+          <Route element={<DefaultLayout />}>
+            <Route path="/" element={<HomePage />}></Route>
+            {/* ROTTE CON IN COMUNE IL PREFISSO  */}
+            <Route path="/posts">
+              <Route path="" element={<PostsPage />}></Route>
+              {/* <Route path=":id" element={<DetailsPostPage />}></Route> */}
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
---
+++ src/pages/posts/PostsPage.jsx
@@ -0,0 +1,10 @@
+import PostsList from "../../components/PostsList";
+
+export default function PostsPage() {
+  return (
+    <>
+      <h1>Pagina posts</h1>
+      <PostsList />
+    </>
+  );
+}
